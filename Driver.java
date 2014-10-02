de5b73bf1cb3c04c4574063e78026e711f44f619
canyounot <canyounot@Yasmeens-MacBook.local>
2014-10-01 23:21:13 -0400

    add driver and shapes

---
+++ Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void  main(String[] args){
+	Shapes s =  new Shapes();
+	System.out.println(s.box(5,4));
+	System.out.println(s.box(4,5));
+	System.out.println(s.tri1(4));
+    }
+}
