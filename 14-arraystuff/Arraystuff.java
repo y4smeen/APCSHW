8d17806f45e81b51a10acb6c5c69cade6809ce17
Yasmeen Roumie <y4smeen@gmail.com>
2014-10-28 08:51:21 -0400

    Create Arraystuff.java

---
+++ 14-arraystuff/Arraystuff.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class  Arraystuff {
+
+    /*--------------------- Instance Variables --------------------*/ 
+
+    private int[] a;
+    Random rnd;
+    // By making x final, we can set it once but then never change it
+    // private final int x = 123;
+
+    /*--------------------- Constructors --------------------*/ 
+
+    public Arraystuff(int n){
+	rnd = new Random();
+	a = new int[n];
+	for (int i=0; i<a.length;i++){
+	    a[i] = 75+rnd.nextInt(76);
+	}
+    }
+
+    public Arraystuff(){
+	this(100);
+    }
+    
+
+    /*--------------------- Methods --------------------*/ 
+
+    public String toString(){
+	String s = "";
+	for (int i = 0; i < a.length; i++) {
+	    s = s + a[i]+", ";
+	}
+	return s;}
+    
+    /*--------------------- Main --------------------*/ 
+
+    public static void main(String[] args) {
+	Arraystuff as = new Arraystuff();
+	System.out.println(as);
+	
+    }
+    
+    public int find(n){
+    
+    }
+    
+}
