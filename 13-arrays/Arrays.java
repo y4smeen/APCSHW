016f3c43a00893960236da34828352528ad8d62e
Yasmeen Roumie <y4smeen@gmail.com>
2014-10-28 09:03:00 -0400

    Update Arrays.java

--- 13-arrays/Arrays.java
+++ 13-arrays/Arrays.java
@@ -11,3 +11,14 @@ public int[] frontPiece(int[] nums) {
   return nums;
   }
 }
+
+public int sum13(int[] nums) {
+int sum=0;
+  for(int i=0;i<nums.length;i++){
+  if(nums[i]!=13){
+  sum += nums[i];
+  }
+  else i++;
+  }
+  return sum;
+}
