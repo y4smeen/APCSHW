60bbf6b08f2b03629fd461056d4de9ba10d9d5d1
Yasmeen Roumie <y4smeen@gmail.com>
2014-10-31 08:25:27 -0400

    Update Arraystuffb.java

--- 14-arraystuff/Arraystuffb.java
+++ 14-arraystuff/Arraystuffb.java
@@ -54,4 +54,13 @@ public boolean tripleUp(int[] nums) {
   }
   return x;
 }
-
+ public int[] seriesUp(int n) {
+	int[] x = new int[n*(n+1)/2];
+	int y = 0;
+	for (int i=0;i <= n; i++) {
+	    for (int j = 1; j <=i; j++, y++) {
+		x[y] = j;
+	    }
+	}
+	return x;
+  }
