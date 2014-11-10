b8727f90f45714c178857fc32dc9718d01a383e1
Yasmeen Roumie <y4smeen@gmail.com>
2014-11-10 09:00:06 -0500

    Update Sarray.java

--- 15-sarray/Sarray.java
+++ 15-sarray/Sarray.java
@@ -54,46 +54,46 @@ public class Sarray{
   
   public int get(int index){
     //returns the item at location index of the list
-    if (index < last && index >= 0) {
-	    return data[index];
-	  } 
-	  else {
-	    return -1;
-	  }
+	if(index<last&&index>=0){
+		return data[index];
+	} 
+	else {
+		return -1;
+    	}
   }
   
   public int set(int index, int i){
     //sets the item at location index to value i
     //returns the old value
-    if (index < last && index >= 0) {
-	    int buffer = data[index];
-	    data[index] = i;
-	    return buffer;
-	  } 
-	  else {
-	    return -1;
+	if(index<last&&index>=0){
+		int buffer=data[index];
+	    	data[index]=i;
+	    	return buffer;
+	} 
+	else{
+	    	return -1;
   	}
   }
   
   public int remove(int index){
     //removes the item at index i
     //returns the old value
-    	if (index < last && index >= 0){
-	    int buffer = data[index];
-	    last -= 1;
-	    for(int counter = index; counter < data.length; counter ++) {
-		  if (counter < last) {
-		    data[counter] = data[counter + 1];
-		  } 
-		  else {
-		    data[counter] = 0;
-		  }
-	    }
-	    return buffer;
+	if(index<last&&index>=0){
+		int buffer=data[index];
+		last-=1;
+		for(int counter=index;counter<data.length;counter ++){
+			if(counter<last){
+				data[counter]=data[counter+1];
+			} 
+			else{
+		    	data[counter]=0;
+			}
+	    	}
+	    	return buffer;
     	}
-	    else {
-	    return -1;
-	    }
+	else{
+		return -1;
+	}
   }
   
   
