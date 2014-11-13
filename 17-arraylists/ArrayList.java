07be8696f2add92a681aefa71ef52057a5c863b4
y4smeen <y4smeen@gmail.com>
2014-11-13 09:25:40 -0500

    add stuff

---
+++ 17-arraylists/ArrayList.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class ArrayList {
+    public static void main(String[] args) {
+			
+		ArrayList<Integer> ai = new ArrayList<Integer>();
+
+		Random r = new Random();
+
+		for (int i = 0; i < 20; i++) {
+				ai.add(i);
+		}
+
+		List<Integer> x = new ArrayList<Integer>(ai);
+
+		for (int j = 0; j < ai.size(); j++) {
+
+		
+		    
+    }
+}
